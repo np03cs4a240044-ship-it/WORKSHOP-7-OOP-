94626d6a77aac7cdf33c8da450f5c97e7236d2ab
np03cs4a240044-ship-it <np03cs4a240044@heraldcollege.edu.np>
2025-12-29 14:02:03 +0545

    Hello

--- student.java
+++ student.java
@@ -1,10 +1,33 @@
 package workshop7;
 
-public class student {
+class Student1 {
+    public String name;
+    public int age;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // Constructor
+    public Student1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-	}
+    // Getter for name
+    public String getName() {
+        return name;
+    }
 
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
 }
+
+public class student {
+    public static void main(String[] args) {
+        Student1 s = new Student1("Alex", 20);
+
+        // Now you can use the getters:
+        System.out.println("Name: " + s.getName());
+        System.out.println("Age: " + s.getAge());
+    }
+}
+
