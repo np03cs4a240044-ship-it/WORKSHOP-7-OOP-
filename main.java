6b8eafff8c3ca4196feb9b2151db397af5e1fef2
np03cs4a240044-ship-it <np03cs4a240044@heraldcollege.edu.np>
2025-12-29 13:50:17 +0545

    Added

---
+++ main.java
@@ -0,0 +1,10 @@
+package workshop7;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("WEEK-8");
+
+	}
+
+}
---
+++ student.java
@@ -0,0 +1,10 @@
+package workshop7;
+
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
